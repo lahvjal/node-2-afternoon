9005b75a5cb5063e19aa24e3b15635c15e1278ca
lahvjal <lahvjal@gmail.com>
2019-02-23 11:48:54 -0700

    PUSH 2

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const massive = require("massive");
 const cors = require("cors");
 require("dotenv").config();
 
-const { PORT } = process.env;
+//destruct variables from .env
+const { PORT, CONNECTION_STRING } = process.env;
 
+//middleware
 const app = express();
-
 app.use(bodyParser.json());
+
+//database connection
+massive();
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT} fool!`);
